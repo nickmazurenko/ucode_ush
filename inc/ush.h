2f7c42fbb587d046dac4a42dd495fc4751e3fb1d
Mykyta Mazurenko <mmazurenko@c1r7p14.khpi.ucode-connect.study>
2021-12-29 19:56:49 +0200

    add headers

--- inc/ush.h
+++ inc/ush.h
@@ -1,3 +1,24 @@
-#pragma once
+#ifndef USH_H
+#define USH_H
 
 #include "libmx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <time.h>
+#include <pwd.h>
+#include <string.h>
+#include <limits.h>
+#include <termios.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <regex.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <spawn.h>
+#include <libgen.h>
+#include <wordexp.h>
+
+#endif
