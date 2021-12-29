224383578fcd276b1fd3fdef9d7c22860ab96339
Mykyta Mazurenko <mmazurenko@c1r7p14.khpi.ucode-connect.study>
2021-12-29 20:12:41 +0200

    change main

--- src/main.c
+++ src/main.c
@@ -1,7 +1,10 @@
 #include "ush.h"
 
 int main() {
-    mx_printstr("HELLO USH!!!");
-
-    return 0;
+    
+    char *buffer = mx_strnew(1);
+    size_t size = 0;
+    getline(&buffer, &size, stdin);
+    mx_printstr(buffer);
+    
 }
